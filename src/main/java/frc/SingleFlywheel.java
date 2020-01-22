10f7475c54fe6f370ee6da725b860843cca98c19
rightshiftoverflow <cocoanut99@gmail.com>
2020-01-22 09:40:08 -0500

    Add docs

--- src/main/java/frc/SingleFlywheel.java
+++ src/main/java/frc/SingleFlywheel.java
@@ -6,6 +6,9 @@ import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Encoder;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ *	Control a single flywheel mechanism
+ */
 public class SingleFlywheel
 {
 	private PIDController pidc;
@@ -16,7 +19,7 @@ public class SingleFlywheel
 	private double optimalSpeed;
 
 	/**
-	 *	Initialize a new SingleFlywheel class
+	 *	Initialize a new SingleFlywheel
 	 *	@param port port number of the motor driving the device
 	 *	@param controller the type of SpeedController
 	 *	@param optimalSpeed the speed at which the motor should run in rotations per second
@@ -31,11 +34,17 @@ public class SingleFlywheel
 		this.atSpeed = new AtomicBoolean(false);
 	}
 
+	/**
+	 *	Start and spin flywheel up to optimal speed
+	 */
 	public void spinUp()
 	{
 		driver.start();
 	}
 
+	/**
+	 *	Stop motor power and allow flywheel to spin down
+	 */
 	public void spinDown()
 	{
 		driver.interrupt();
@@ -47,7 +56,7 @@ public class SingleFlywheel
 	public boolean atSpeed() { return this.atSpeed.get(); }
 
 	/*
-	 *	This is a temporary class to do demonstation on.
+	 *	This is a temporary class to do demonstration on.
 	 *	I feel that the current auto driving needs improvement
 	 *	that can't be done right now, so in the meantime, this
 	 *	will work.
