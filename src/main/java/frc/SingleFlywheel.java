02f9cbc0007b2c3259e12199c802cd320c10fc50
rightshiftoverflow <cocoanut99@gmail.com>
2020-01-25 09:35:28 -0500

    Remove FlywheelDriver
    Remove FlywheelDriver because we no longer need it, the CANSparkMax has a built in PID controller, so all we have to do is set it and forget it.

--- src/main/java/frc/SingleFlywheel.java
+++ src/main/java/frc/SingleFlywheel.java
@@ -1,8 +1,6 @@
 package frc;
 
 import org.usfirst.frc.team6500.trc.util.TRCTypes;
-import java.util.concurrent.atomic.AtomicBoolean;
-
 import com.revrobotics.*;
 
 /**
@@ -11,22 +9,17 @@ import com.revrobotics.*;
 public class SingleFlywheel
 {
 	private CANSparkMax drive;
-	private FlywheelDriver driver;
-	private AtomicBoolean atSpeed;
 	private double optimalSpeed;
 
 	/**
 	 *	Initialize a new SingleFlywheel
 	 *	@param port port number of the SparkMax driving the Neo
-	 *	@param controller the type of SpeedController
 	 *	@param optimalSpeed the speed at which the motor should run in rotations per second
 	 */
 	public SingleFlywheel(int port, double optimalSpeed)
 	{
 		drive = new CANSparkMax(port, CANSparkMaxLowLevel.MotorType.kBrushless);
-		driver = new FlywheelDriver();
 		this.optimalSpeed = optimalSpeed;
-		this.atSpeed = new AtomicBoolean(false);
 		/* // code here to tune PID controller
 		CANPIDController pidc = drive.getPIDController();
 		pidc.setP(0.2);
@@ -40,7 +33,7 @@ public class SingleFlywheel
 	 */
 	public void spinUp()
 	{
-		driver.start();
+		drive.getPIDController().setReference(optimalSpeed, ControlType.kVelocity);
 	}
 
 	/**
@@ -48,36 +41,11 @@ public class SingleFlywheel
 	 */
 	public void spinDown()
 	{
-		driver.interrupt();
+		drive.stopMotor();
 	}
 
 	/**
 	 *	@return if flywheel is spinning at optimal speed
 	 */
-	public boolean atSpeed() { return this.atSpeed.get(); }
-
-	/*
-	 *	This is a temporary class to do demonstration on.
-	 *	I feel that the current auto driving needs improvement
-	 *	that can't be done right now, so in the meantime, this
-	 *	will work.
-	 */
-	private class FlywheelDriver extends java.lang.Thread
-	{
-		@Override
-		public void run()
-		{
-			CANPIDController pidc = drive.getPIDController();
-			pidc.setReference(optimalSpeed, ControlType.kVelocity);
-			atSpeed.set(pidc.getIAccum() >= pidc.getIZone()); // TODO: problably not right
-		}
-
-		@Override
-		public void interrupt()
-		{
-			super.interrupt();
-			atSpeed.set(false);
-			drive.stopMotor();
-		}
-	}
+	public boolean atSpeed() { return drive.getPIDController().getIAccum() >= drive.getPIDController().getIZone(); }
 }
